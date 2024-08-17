17f856cc4419127cc3ecee5d416f19c8284ebd80
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2024-08-17 21:38:09 +0400

    node js with express docker image created

--- express-js/server.js
+++ express-js/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 3001;
+const port = 3300;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(`Hello World! from port=${port}`);
 });
 
 app.listen(port, () => {
